e2cafa393c3bc78cf81835e79b2125768d72093b
zdddddd <470015321@qq.com>
2018-07-14 02:29:18 +0800

    xinjian

--- src/main/java/com/run/RunTest.java
+++ src/main/java/com/run/RunTest.java
@@ -11,7 +11,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
                 "rerun:target/site/rerun.txt",
                 "json:target/cucumberjson.json"
         },
-        tags = {"~@p2"},
+        tags = {"@p1"},
         glue = {"com.steps"}
 )
 
