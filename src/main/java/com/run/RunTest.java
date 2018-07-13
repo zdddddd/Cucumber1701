bf341b737ce5c165cc88b06036a470e6a7515851
zdddddd <470015321@qq.com>
2018-07-14 02:07:30 +0800

    xinjian

---
+++ src/main/java/com/run/RunTest.java
@@ -0,0 +1,21 @@
+package com.run;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+@CucumberOptions(
+        features = "src/test/features/first_cucumber.feature",
+        format = {"pretty",
+                "html:target/site/cucumber-pretty",
+                "rerun:target/site/rerun.txt",
+                "json:target/cucumberjson.json"
+        },
+        tags = {"~@p2"},
+        glue = {"com.steps"}
+)
+
+public class RunTest extends AbstractTestNGCucumberTests {
+
+
+}
